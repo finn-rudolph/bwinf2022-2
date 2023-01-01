7a1ed7939e63039af041f6d9c0b1b54bc2c0e69f
Finn Rudolph <finn.rudolph@t-online.de>
2023-01-01 12:05:59 +0100

    A3: Implement branch and bound for a)

--- aufgabe3/aufgabe3_a.cpp
+++ aufgabe3/aufgabe3_a.cpp
@@ -20,7 +20,7 @@ unsigned lbound(vector<unsigned> const &p)
     bool incr = p[1] > p[0];
     unsigned num_monotone = 1;
 
-    for (size_t i = 1; i < p.size(); i++)
+    for (size_t i = 2; i < p.size(); i++)
         incr = is_increasing(incr, p[i - 1], p[i], num_monotone);
 
     return (num_monotone + 1) / 3;
@@ -36,12 +36,12 @@ unsigned lbound_gamma(vector<unsigned> const &p, size_t i)
     bool incr =
         i >= 2 ? (p[i - 2] > p[i - 1])
                : (i == 1 ? (p[i + 1] > p[i - 1]) : (p[i + 2] > p[i + 1]));
-    unsigned num_monotone = 0;
+    unsigned num_monotone = 1;
 
     for (size_t j = 2; j < i; j++)
         incr = is_increasing(incr, p[i - j], p[i - j - 1], num_monotone);
 
-    if (i && i < p.size())
+    if (i && i + 1 < p.size())
         incr = is_increasing(incr, p[0], p[i + 1], num_monotone);
 
     for (size_t j = i + 2; j < p.size(); j++)
@@ -50,9 +50,43 @@ unsigned lbound_gamma(vector<unsigned> const &p, size_t i)
     return (num_monotone + 1) / 3;
 }
 
-vector<unsigned> shortest_ops(vector<unsigned> const &p)
+pair<vector<unsigned>, bool> shortest_ops(
+    vector<unsigned> const &p, vector<unordered_set<size_t>> &vis,
+    unsigned b = UINT_MAX)
 {
-    vector<unsigned> bounds(p.size());
+    if (!ind(p))
+        return {{}, 1};
+
+    if (vis[p.size() - 1].find(ind(p)) != vis[p.size() - 1].end())
+        return {{}, 0};
+
+    priority_queue<
+        pair<unsigned, size_t>, vector<pair<unsigned, size_t>>,
+        greater<pair<unsigned, size_t>>>
+        q;
+    for (size_t i = 0; i < p.size(); i++)
+        q.push({lbound_gamma(p, i), i});
+
+    vector<unsigned> ops;
+    bool found_better = 0;
+
+    while (!q.empty() && q.top().first < b)
+    {
+        auto [res, found] = shortest_ops(gamma(p, q.top().second), vis, b - 1);
+
+        if (found && res.size() + 1 < b)
+        {
+            b = res.size() + 1;
+            ops = res;
+            ops.push_back(q.top().second);
+            found_better = 1;
+        }
+
+        q.pop();
+    }
+
+    vis[p.size() - 1].insert(ind(p));
+    return {ops, found_better};
 }
 
 int main()
@@ -66,4 +100,19 @@ int main()
         cin >> x;
         x--;
     }
+
+    precalc_factorial(n);
+    vector<unordered_set<size_t>> vis(n);
+    vector<unsigned> ops = shortest_ops(p, vis).first;
+
+    if (!ops.empty())
+    {
+        cout << ops.size() << " Operationen nötig. Dafür hinter folgenden"
+                              " Indizes wenden:\n";
+        for (auto it = ops.rbegin(); it != ops.rend(); it++)
+            cout << *it << ' ';
+    }
+    else
+        cout << "Der Stapel ist bereits sortiert.";
+    cout << '\n';
 }
