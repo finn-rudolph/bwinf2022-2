4c5a8930356c1f96a226b411224886834195eec6
Finn Rudolph <finn.rudolph@t-online.de>
2023-01-02 21:05:49 +0100

    A3: Change ith_permutation to O(n log n)

--- aufgabe3/util.cpp
+++ aufgabe3/util.cpp
@@ -2,16 +2,6 @@
 #include "util.hpp"
 using namespace std;
 
-vector<__uint128_t> factorial;
-
-void precalc_factorial(size_t n)
-{
-    factorial = vector<__uint128_t>(n + 1);
-    factorial[0] = 1;
-    for (size_t i = 1; i <= n; i++)
-        factorial[i] = factorial[i - 1] * i;
-}
-
 size_t ind(vector<unsigned> const &p)
 {
     size_t k = 0;
@@ -107,57 +97,34 @@ vector<unsigned> gamma_inv(vector<unsigned> const &p, size_t i, unsigned r)
     return q;
 }
 
-size_t ind_gamma_inv(vector<unsigned> const &p, size_t i, unsigned r)
-{
-    assert(factorial.size() >= p.size());
-    assert(i < p.size() + 1);
-    assert(r <= p.size() + 1);
-
-    size_t k = 0;
-    uint64_t mask = 0;
-
-    for (size_t j = 0; j < i; j++)
-    {
-        unsigned const x = p[i - j - 1] + (p[i - j - 1] >= r);
-        k += (x - __builtin_popcount(mask >> (p.size() - x))) *
-             factorial[p.size() - j];
-        mask ^= 1ULL << (p.size() - x);
-    }
-
-    k += (r - __builtin_popcount(mask >> (p.size() - r))) *
-         factorial[p.size() - i];
-    mask ^= 1ULL << (p.size() - r);
-
-    for (size_t j = i + 1; j < p.size() + 1; j++)
-    {
-        unsigned const x = p[j - 1] + (p[j - 1] >= r);
-        k += (x - __builtin_popcount(mask >> (p.size() - x))) *
-             factorial[p.size() - j];
-        mask ^= 1ULL << (p.size() - x);
-    }
-
-    assert(mask == (1ULL << (p.size() + 1)) - 1);
-    return k;
-}
-
 void ith_permutation(size_t n, size_t i, vector<unsigned> &p)
 {
-    assert(factorial.size() >= n);
-    assert(i < factorial[n]);
-    assert(p.size() >= n);
-
     // Berechne die Ziffern von i im fakultätsbasierten Zahlensystem.
-    for (size_t j = 0; j < n; j++)
+    for (size_t j = 1; j <= n; j++)
     {
-        p[j] = i / factorial[n - j - 1];
-        i %= factorial[n - j - 1];
+        p[n - j] = i % j;
+        i /= j;
     }
 
-    // Addiere zu jeder Ziffer die Anzahl kleiner oder gleicher, links gelegener
-    // Ziffern.
-    for (size_t j = n - 1; j; j--)
-        for (size_t k = j - 1; k < n; k--)
-            p[j] += (p[k] <= p[j]);
+    size_t const lgn = 32 - __builtin_clz(p.size()), m = 1 << lgn;
+    unsigned tree[2 * m];
+    for (size_t l = 0; l <= lgn; l++)
+        for (size_t j = 0; j < (1ULL << l); j++)
+            tree[(1 << l) + j] = 1 << (lgn - l);
+
+    for (size_t j = 0; j < n; j++)
+    {
+        size_t z = 1;
+        for (size_t l = 0; l < lgn; l++)
+        {
+            tree[z]--;
+            z <<= 1;
+            if (p[j] >= tree[z])
+                p[j] -= tree[z++];
+        }
+        tree[z] = 0;
+        p[j] = z - m;
+    }
 }
 
 vector<unsigned> ith_permutation(size_t n, size_t i)
--- aufgabe3/util.hpp
+++ aufgabe3/util.hpp
@@ -4,8 +4,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void precalc_factorial(size_t n);
-
 size_t ind(vector<unsigned> const &p);
 
 vector<unsigned> gamma(vector<unsigned> const &p, size_t i);
@@ -13,8 +11,6 @@ vector<unsigned> gamma(vector<unsigned> const &p, size_t i);
 size_t ind_gamma(vector<unsigned> const &p, size_t i);
 
 vector<unsigned> gamma_inv(vector<unsigned> const &p, size_t i, unsigned r);
-
-size_t ind_gamma_inv(vector<unsigned> const &p, size_t i, unsigned j);
 
 void ith_permutation(size_t n, size_t i, vector<unsigned> &p);
 
