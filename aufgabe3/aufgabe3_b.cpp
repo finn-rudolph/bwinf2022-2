908c01ba03e8ee79f5655e8bfe79c9ae99e896b5
Finn Rudolph <finn.rudolph@t-online.de>
2023-01-04 20:54:40 +0100

    A3: Parallelize solution for b)

--- aufgabe3/aufgabe3_b.cpp
+++ aufgabe3/aufgabe3_b.cpp
@@ -2,6 +2,48 @@
 #include "util.hpp"
 using namespace std;
 
+void update_z(
+    size_t k, size_t u, size_t v, uint8_t const *const y, uint8_t *const z,
+    size_t i1, size_t i2)
+{
+    vector<unsigned> p = ith_permutation(k, i1);
+
+    for (size_t i = i1; i < i2; i++)
+    {
+        z[i] = k;
+        z[u - i - 1] = k;
+
+        for (size_t j = 0; j < k; j++)
+        {
+            size_t const l = ind_gamma(p, j);
+            z[i] = min<uint8_t>(z[i], y[l] + 1);
+            z[u - i - 1] = min<uint8_t>(z[u - i - 1], y[v - l - 1] + 1);
+        }
+
+        next_permutation(p.begin(), p.end());
+    }
+}
+
+pair<unsigned, size_t> get_max_a(
+    size_t n, uint8_t const *const y, size_t i1, size_t i2)
+{
+    pair<unsigned, size_t> res = {0, -1};
+    vector<unsigned> p = ith_permutation(n, i1);
+
+    for (size_t i = i1; i < i2; i++)
+    {
+        unsigned a = n;
+
+        for (size_t j = 0; j < n; j++)
+            a = min<unsigned>(a, y[ind_gamma(p, j)] + 1);
+
+        res = max(res, {a, i});
+        next_permutation(p.begin(), p.end());
+    }
+
+    return res;
+}
+
 int main()
 {
     size_t n;
@@ -11,29 +53,22 @@ int main()
             *z = (uint8_t *)malloc(sizeof(uint8_t));
     y[0] = 0;
     size_t u = 1, v = 1;
+    unsigned const n_threads = thread::hardware_concurrency();
 
     for (size_t k = 2; k < n; k++)
     {
         u *= k;
         z = (uint8_t *)realloc(z, u * sizeof(uint8_t));
 
-        vector<unsigned> p(k);
-        for (size_t i = 0; i < k; i++)
-            p[i] = i;
+        vector<thread> threads;
 
-        for (size_t i = 0; i < u / 2; i++)
-        {
-            z[i] = k;
-            z[u - i - 1] = k;
-            for (size_t j = 0; j < k; j++)
-            {
-                size_t const l = ind_gamma(p, j);
-                z[i] = min<uint8_t>(z[i], y[l] + 1);
-                z[u - i - 1] = min<uint8_t>(z[u - i - 1], y[v - l - 1] + 1);
-            }
+        for (unsigned i = 0; i < n_threads; i++)
+            threads.emplace_back(update_z, k, u, v, y, z,
+                                 (u / 2) * i / n_threads,
+                                 (u / 2) * (i + 1) / n_threads);
 
-            next_permutation(p.begin(), p.end());
-        }
+        for (thread &t : threads)
+            t.join();
 
         z[0] = 0;
         swap(y, z);
@@ -41,21 +76,19 @@ int main()
     }
 
     u *= n;
-    unsigned max_a = 0;
-    vector<unsigned> p(n);
-    for (size_t i = 0; i < n; i++)
-        p[i] = i;
+    pair<unsigned, size_t> res = {0, -1};
+    vector<future<pair<unsigned, size_t>>> fut;
 
-    for (size_t i = 0; i < u; i++)
-    {
-        unsigned a = n;
+    for (unsigned i = 0; i < n_threads; i++)
+        fut.emplace_back(
+            async(get_max_a, n, y, u * i / n_threads, u * (i + 1) / n_threads));
 
-        for (size_t j = 0; j < n; j++)
-            a = min<unsigned>(a, y[ind_gamma(p, j)] + 1);
+    for (auto &f : fut)
+        res = max(res, f.get());
 
-        max_a = max(max_a, a);
-        next_permutation(p.begin(), p.end());
-    }
-
-    cout << "P(" << n << ") = " << max_a << '\n';
+    cout << "P(" << n << ") = " << res.first << '\n';
+    cout << "Beispiel mit A(p) = P(" << n << "): ";
+    for (unsigned const x : ith_permutation(n, res.second))
+        cout << x + 1 << ' ';
+    cout << '\n';
 }
