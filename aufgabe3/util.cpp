3db6dc2366e54da35d582fa4e9557be80fb892e0
Finn Rudolph <finn.rudolph@t-online.de>
2023-01-05 17:05:05 +0100

    A3: Describe implementation of util.cpp

--- aufgabe3/util.cpp
+++ aufgabe3/util.cpp
@@ -13,7 +13,7 @@ size_t ind(vector<unsigned> const &p)
 
     for (size_t j = 0; j < p.size(); j++)
     {
-        size_t z = m + p[j];
+        size_t z = m + p[j]; // Aktueller Knoten.
         k *= (p.size() - j);
         k += p[j];
 
@@ -35,14 +35,14 @@ size_t ind(vector<unsigned> const &p)
 
 vector<unsigned> gamma(vector<unsigned> const &p, size_t i)
 {
-    vector<unsigned> q(p.size() - 1);
+    vector<unsigned> s(p.size() - 1);
 
     for (size_t j = 0; j < i; j++)
-        q[j] = p[i - j - 1] - (p[i - j - 1] > p[i]);
+        s[j] = p[i - j - 1] - (p[i - j - 1] > p[i]);
     for (size_t j = i; j < p.size() - 1; j++)
-        q[j] = p[j + 1] - (p[j + 1] > p[i]);
+        s[j] = p[j + 1] - (p[j + 1] > p[i]);
 
-    return q;
+    return s;
 }
 
 // Berechnet den Index von gamma_i p.
@@ -134,11 +134,11 @@ vector<unsigned> ith_permutation(size_t n, size_t i)
 // Die eigentliche Wende-und-Ess-Operation.
 vector<unsigned> rev_and_eat(vector<unsigned> const &p, size_t i)
 {
-    vector<unsigned> q(p.size() - 1);
+    vector<unsigned> s(p.size() - 1);
 
-    copy(p.begin(), p.begin() + i, q.begin());
-    reverse(q.begin(), q.begin() + i);
-    copy(p.begin() + i + 1, p.end(), q.begin() + i);
+    copy(p.begin(), p.begin() + i, s.begin());
+    reverse(s.begin(), s.begin() + i);
+    copy(p.begin() + i + 1, p.end(), s.begin() + i);
 
-    return q;
+    return s;
 }
