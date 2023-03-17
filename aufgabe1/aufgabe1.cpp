dd5cccd2b16471b81d8411c26ed989731080a99b
Finn Rudolph <finn.rudolph@t-online.de>
2023-03-17 20:21:54 +0100

    A1: Add MANY comments

--- aufgabe1/aufgabe1.cpp
+++ aufgabe1/aufgabe1.cpp
@@ -32,8 +32,8 @@ void add_angle_constraints(HighsModel &model, vector<complex<double>> const &z)
                     HighsLp &lp = model.lp_;
                     lp.a_matrix_.index_.push_back(edge_index(z.size(), i, j));
                     lp.a_matrix_.index_.push_back(edge_index(z.size(), j, k));
-                    lp.a_matrix_.value_.push_back(1);
-                    lp.a_matrix_.value_.push_back(1);
+                    lp.a_matrix_.value_.push_back(1); // Der Koeffizient jeder
+                    lp.a_matrix_.value_.push_back(1); // Kante ist 1.
                     lp.row_lower_.push_back(0);
                     lp.row_upper_.push_back(1);
                     lp.a_matrix_.start_.push_back(lp.a_matrix_.index_.size());
@@ -50,14 +50,14 @@ void add_degree_constraints(HighsModel &model, size_t n)
     {
         for (size_t j = 0; j < n; j++)
         {
-            if (i != j)
-            {
+            if (i != j) // Die Kante zu jedem Knoten != i wird mit Koeffizient
+            {           // 1 zur aktuellen Zeile hinzugefügt.
                 model.lp_.a_matrix_.index_.push_back(edge_index(n, i, j));
                 model.lp_.a_matrix_.value_.push_back(1);
             }
         }
-        model.lp_.row_lower_.push_back(1);
-        model.lp_.row_upper_.push_back(2);
+        model.lp_.row_lower_.push_back(1); // Setze Unter- und Oberschranke der
+        model.lp_.row_upper_.push_back(2); // Zeile.
         model.lp_.a_matrix_.start_.push_back(model.lp_.a_matrix_.index_.size());
     }
 }
@@ -79,6 +79,7 @@ void add_num_edges_constraint(HighsModel &model, size_t n)
 void add_subtour_elimination_constraint(
     Highs &highs, size_t n, vector<size_t> const &tour)
 {
+    // Arrays für die neuen Spaltenindizes und Werte.
     HighsInt *ind = (HighsInt *)malloc(nchoose2(tour.size()) * sizeof *ind);
     double *val = (double *)malloc(nchoose2(tour.size()) * sizeof *val);
     size_t k = 0; // Anzahl bereits in ind bzw. val eingefügter Elemente
@@ -93,8 +94,8 @@ void add_subtour_elimination_constraint(
         }
     }
 
-    // Verwende Highs::addRow(double lower, double upper, HighsInt num_new_nz,
-    //                        const HighsInt *indices, const double *values)
+    // Verwendet Highs::addRow(double lower, double upper, HighsInt num_new_nz,
+    //                         const HighsInt *indices, const double *values)
     HighsStatus status;
     status = highs.addRow(0, tour.size() - 1, nchoose2(tour.size()), ind, val);
     assert(status == HighsStatus::kOk);
@@ -135,14 +136,14 @@ bool check_for_subtours(Highs &highs, size_t n)
                 visited[j] = 1;
                 subtour.push_back(j);
                 size_t next = SIZE_MAX;
-                for (size_t k : graph[j])
-                    if (k != last)
-                        next = k;
-                last = j;
+                for (size_t k : graph[j]) // Da der Grad jedes Knoten <= 2 ist,
+                    if (k != last)        // wurde jeder Knoten unterschiedlich
+                        next = k;         // zum letzten noch nicht besucht oder
+                last = j;                 // ein Zyklus gefunden.
                 j = next;
             } while (j != i && j != SIZE_MAX);
 
-            if (j == i)
+            if (j == i) // Zyklus gefunden.
             {
                 has_subtours = 1;
                 add_subtour_elimination_constraint(highs, n, subtour);
@@ -167,8 +168,9 @@ pair<vector<complex<double>>, double> get_optimal_tour(
     model.lp_.a_matrix_.start_ = {0}; // Die erste Zeile beginnt bei Index 0.
     model.lp_.num_col_ = nchoose2(n);
 
+    // Füge die Länge jeder Kante als ihren Koeffizienten zur Kostenfunktion
+    // hinzu und beschränke ihre Variable auf 0 oder 1.
     for (size_t i = 0; i < n; i++)
-    {
         for (size_t j = i + 1; j < n; j++)
         {
             model.lp_.col_cost_.push_back(abs(z[i] - z[j]));
@@ -176,7 +178,6 @@ pair<vector<complex<double>>, double> get_optimal_tour(
             model.lp_.col_lower_.push_back(0);
             model.lp_.col_upper_.push_back(1);
         }
-    }
 
     add_angle_constraints(model, z);
     add_degree_constraints(model, n);
@@ -192,13 +193,13 @@ pair<vector<complex<double>>, double> get_optimal_tour(
 
     while (has_subtours && model_status != HighsModelStatus::kInfeasible)
     {
-        status = highs.run();
+        status = highs.run(); // Löse das ILP.
         assert(status == HighsStatus::kOk);
         model_status = highs.getModelStatus();
         has_subtours = check_for_subtours(highs, n);
     }
 
-    if (model_status == HighsModelStatus::kInfeasible)
+    if (model_status == HighsModelStatus::kInfeasible) // Kein Tour möglich.
         return make_pair(vector<complex<double>>(), 0.0);
 
     vector<vector<size_t>> graph = build_graph(highs, n);
